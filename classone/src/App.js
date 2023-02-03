a005ff3883a82479e813178b8fb2e2b643796d43
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2023-02-03 21:36:51 +0500

    3-02-2023

--- classone/src/App.js
+++ classone/src/App.js
@@ -1,129 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let a = 10;
-  let arr = [1, 2, 3, 4];
-  let mobiles = [
-    {
-      name: "a30",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 2000,
-      brand: "samsung",
-    },
-    {
-      name: "note10",
-      ram: "6gb",
-      rom: "128gb",
-      camera: "50px",
-      price: 4000,
-      brand: "samsung",
-    },
-    {
-      name: "s10",
-      ram: "3gb",
-      rom: "128gb",
-      camera: "10px",
-      price: 5000,
-      brand: "samsung",
-    },
-    {
-      name: "iphone4",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "iphone",
-    },
-    {
-      name: "iphone4s",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 20000,
-      brand: "iphone",
-    },
-    {
-      name: "iphone5",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 30000,
-      brand: "iphone",
-    },
-    {
-      name: "iphone6",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 30000,
-      brand: "iphone",
-    },
-    {
-      name: "redminote10",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "redminote11",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "redmi10",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "redmi10pro",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "xiaomi",
-    },
-    {
-      name: "a3s",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "oppo",
-    },
-    {
-      name: "a3s",
-      ram: "4gb",
-      rom: "64gb",
-      camera: "50px",
-      price: 10000,
-      brand: "Realmi",
-    },
-  ];
+  const [count, setCount] = useState(0);
+  const [arr, setArr] = useState(["akjhsd", "asjhdkl"]);
+
+  const [text, setText] = useState("");
+
+  let add = () => {
+    arr.push(text);
+    console.log(arr);
+    setArr([...arr]);
+  };
+
+  let edit = (i)=>{
+    // get prompt
+    // arr[i] == prompt value
+    // set State of Array
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello React</h1>
-        {mobiles.map((x, i) => {
-          return (
-            <div key={i} className="card">
-              <h3>{x.brand}</h3>
-              <p>{x.name}</p>
-              <p>Rs {x.price} /-</p>
-            </div>
-          );
-        })}
+        {arr.map((x, i) => (
+          <p>{x} <button onClick={()=>del(i)}></button></p>
+        ))}
+
+        <input
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          placeholder="Enter Text"
+        />
+
+        <p>{text}</p>
+
+        <button onClick={add}>Add</button>
       </header>
     </div>
   );
