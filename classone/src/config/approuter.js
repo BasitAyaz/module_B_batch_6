2982464ca3b2bd6dafaff25aa1a72a02536fc52e
BasitAyaz <basitahmed1997@gmail.com>
2023-02-24 21:36:22 +0500

    24-2-2023 (dynamic routes)

--- classone/src/config/approuter.js
+++ classone/src/config/approuter.js
@@ -1,18 +1,23 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import About from "../screens/about";
 import Home from "../screens/home";
+import About from "../screens/about";
+import Posts from "../screens/post";
+import SinglePost from "../screens/singlepost";
 
 export default function AppRouter() {
   return (
     <>
       <BrowserRouter>
         <nav>
-          <Link to="/home">Home Page</Link>
-          <Link to="/about">About Page</Link>
+          <Link to="/">Home</Link>
+          <Link to="about">About</Link>
+          <Link to="post">Posts</Link>
         </nav>
         <Routes>
-          <Route path="home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="post" element={<Posts />} />
+          <Route path="singlepost/:id" element={<SinglePost />} />
         </Routes>
       </BrowserRouter>
     </>
